08c080beec9f9070253293973307ca70e233dbe6
plectrum0224 <plectrum@outlook.com>
2016-03-06 20:37:28 +0800

    JavaScript学习

--- Chapter1/scripts/gallery.js
+++ Chapter1/scripts/gallery.js
@@ -3,17 +3,45 @@
  */
 
 function showPic(whichPic) {
+    if(!document.getElementById("placeholder")) return false;
     var source = whichPic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
+    if(document.getElementById("description")){
+        var text = whichPic.getAttribute("title");
+        var desc = document.getElementById("description");
+        desc.firstChild.nodeValue = text;
+    }
+    return true
 }
 
-function countBodyChildren(){
-    var text = whichPic.getAttribute("title");
-    var body_element = document.getElementsByTagName("body")[0];
-    alert(body_element.childNodes.length);
+function prepareGallery(){
+    if(!document.getElementsByTagName||!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i=0; i<links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this) ? false:true;
+        }
+    }
 }
 
-window.onload = countBodyChildren;
+function addLoadEvent(func){
+    var oldOnload = window.onload;
+    if(typeof window.onload != 'function'){
+        window.onload = func;
+    }else {
+        window.onload = function(){
+            oldOnload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
+
+
+
 
 
